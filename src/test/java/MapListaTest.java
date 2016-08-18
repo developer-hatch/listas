49defa612815a8a3556c87703d80d712c0c11baa
damian <damianlattenero@gmail.com>
2016-08-18 09:53:42 -0300

    Test de map

---
+++ src/test/java/MapListaTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * Created by damian on 18/08/16.
+ */
+public class MapListaTest {
+    @Test
+    public void sumarUnoATodos() {
+        MiLista lista = new MiLista(Arrays.asList(2, 3, 5, 6, 8, 9));
+
+        lista.sumarUnoATodos();
+
+        Assert.assertEquals(Arrays.asList(3, 4, 6, 7, 9, 10), lista.lista);
+
+    }
+
+}
